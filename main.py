8ec36a97c83bd43138f0025f429edd2840f3b54b
T14-1182 <lyubimova.t@bk.ru>
2024-09-25 12:08:46 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+def apply_all_func(int_list, *functions):
+    results = {funk.__name__: funk(tuple(int_list)) for funk in functions}
+    return results
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
